086eb9fdc837c9f38cc804b5c7977674a7ae5df2
David De Vargas <david.devargas@zemoga.com>
2022-05-27 14:33:26 -0500

    feat: Layout & Navbar implementation

---
+++ components/layouts/Layout.tsx
@@ -0,0 +1,27 @@
+import { FC, ReactNode } from "react";
+import Head from 'next/head';
+import { Box } from "@mui/material"
+
+import { Navbar } from "../ui";
+
+interface Props {
+    title?: string;
+    children?: ReactNode;
+}
+
+export const Layout: FC<Props> = ({ title = 'OpenJira', children }) => {
+  return (
+    <Box sx={{ flexFlow: 1 }}>
+        <Head>
+            <title>{ title }</title>
+        </Head>
+
+        <Navbar />
+        {/* sidebar */}
+
+        <Box sx={{ addingTop: '10px 20px' }}>
+            { children }
+        </Box>
+    </Box>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,13 @@
-import { Typography } from '@mui/material'
 import type { NextPage } from 'next'
+import { Typography } from '@mui/material'
+
+import { Layout } from '../components/layouts'
 
 const HomePage: NextPage = () => {
   return (
-    <Typography variant='h1' color='primary'>Hola mundo!</Typography>
+    <Layout>
+      <Typography variant='h1' color='primary'>Hola mundo!</Typography>
+    </Layout>
   )
 }
 
