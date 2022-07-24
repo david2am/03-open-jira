3913d02db225a5047b162cbe77adc6df8574e5af
David De Vargas <david.devargas@zemoga.com>
2022-07-24 18:54:25 -0500

    setup(db): connection

--- models/Entry.ts
+++ models/Entry.ts
@@ -11,7 +11,8 @@ const EntrySchema = new Schema({
         enum: {
             values: [ 'pending', 'in-progress', 'finished' ],
             message: '{VALUE} is not allowed'
-        }
+        },
+        default: 'pending'
     }
 })
 
--- pages/api/entries/index.ts
+++ pages/api/entries/index.ts
@@ -4,30 +4,58 @@ import { Entry, IEntry } from '../../../models'
 
 type Method = 'GET' | 'POST' | 'PUT' |  'DELETE'
 type Request = {
-    [Property in Method]?: ( res: NextApiResponse<Data> ) => Promise<void>
+    [Property in Method]?: (( req: NextApiRequest, res: NextApiResponse<Data> ) => Promise<void>)
 }
 
 type Data = 
     | { message: string }
     | IEntry[]
+    | IEntry
 
 
 const getEntries = async ( res: NextApiResponse<Data> ) => {
-    await db.connect()
-    const entries = await Entry.find().sort({ createdAt: 'asc' })
-    await db.disconnect()
-    
-    res.status(200).json(entries)
+    try {
+        await db.connect()
+        const entries = await Entry.find().sort({ createdAt: 'asc' })
+        await db.disconnect()
+        
+        res.status(200).json(entries)
+    } catch (error) {
+        await db.disconnect()
+        res.status(500).json({ message: 'Something went wrong' })
+    }
+}
+
+const addEntry = async (req: NextApiRequest, res: NextApiResponse<Data> ) => {
+    const { body: { description = '' } } = req
+
+    const newEntry = new Entry({
+        description,
+        createdAt: Date.now(),
+    })
+
+    try {
+        await db.connect()
+        await newEntry.save()
+        await db.disconnect()
+        
+        res.status(201).json( newEntry )
+    } catch (error) {
+        await db.disconnect()
+        res.status(500).json({ message: 'Something went wrong' })
+    }
 }
 
 
 const request: Request = {
-    'GET': (res: NextApiResponse<Data>) => getEntries(res)
+    'GET':  ( req, res ) => getEntries(res),
+    'POST': ( req, res ) => addEntry(req, res),
 }
 
 export default function handler (req: NextApiRequest, res: NextApiResponse<Data>) {
-    if (req.method && request[req.method as Method])
-        return request[req.method as Method]?.( res )
+    if (req.method && request[req.method as Method]) {
+        return request[req.method as Method]?.( req, res )
+    }
     return res.status(400).json({ message: 'Endpoint don\'t exist' })
 
     // switch ( req.method ) {
